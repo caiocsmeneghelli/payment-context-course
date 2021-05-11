7783f18d3eaf93f7d929bc402f54c09c38bba71b
Caio Cesar <caiocsmeneghelli@outlook.com>
2021-05-11 11:35:05 -0300

    Corrupção no codigo

---
+++ PaymentContext.Tests/Entities/StudentTests.cs
@@ -0,0 +1,22 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PaymentContext.Domain.Entities;
+
+namespace PaymentContext.Tests
+{
+    [TestClass]
+    public class StudentTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var subscription = new Subscription(null);
+            var student = new Student(
+                "Caio Cesar",
+                "Serrano Meneghelli",
+                "14905755700",
+                "caiocsmeneghelli@outlook.com"
+            );
+            student.AddSubscription(subscription);
+        }
+    }
+}
